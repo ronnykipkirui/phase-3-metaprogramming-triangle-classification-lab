b42df239f0e8bc6b48fa05c94cf5cdcea7b656aa
Ronny <ronnybet41@gmail.com>
2022-10-26 15:17:55 +0300

    completed assignment

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,34 @@
 class Triangle
   # write code here
+  def initialize a, b, c
+    @a=a
+    @b=b
+    @c=c
+  end
+
+  def kind 
+    if (@a>0 and @b>0 and @c>0 and (@a+@b)>@c and (@b+@c)>@a and (@a+@c)>@b)
+      if @a==@b and @b==@c and @a==@c 
+        return :equilateral
+      elsif (@a==@b ||@a==@c ||@b==@c)
+        return :isosceles
+      elsif (@a!=@b and @b!=@c and @a!=@c)
+        return :scalene
+      end
+    
+    else
+      begin
+        raise TriangleError
+      end
+    end
+
+  end
+
+  class TriangleError < StandardError
+    def message
+      "invalid triangle"
+    end
+  end
 end
+
+puts Triangle.new(4,4,4).kind
